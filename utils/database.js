0223594f32fb4d18ffbff1497a7f04623f5bdfce
Zouheir Ghosn <zouzoughosn99@gmail.com>
2025-04-17 21:41:53 +0300

    deleted more files

--- utils/database.js
+++
@@ -1,8 +0,0 @@
-// utils/database.js
-const db = require('quick.db'); // or any other database module you are using
-
-module.exports = {
-    get: (key) => db.get(key),  // Retrieve a value from the database
-    set: (key, value) => db.set(key, value),  // Set a value in the database
-    delete: (key) => db.delete(key),  // Delete a value from the database
-};
